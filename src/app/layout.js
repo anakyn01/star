ff14fd85d5abf6ec10b0ffdb38f2effc75b990d9
hwang <anakyn@naver.com>
2025-06-30 21:58:16 +0900

    dd

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,8 +15,7 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         {children}
-                   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> 
-<script src="/star/js/custom.js"></script>
+
       </body>
     </html>
   );
