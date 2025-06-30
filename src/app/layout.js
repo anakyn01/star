5abb2b9fd0ec5512eff6e0c7b7755d724038e392
hwang <anakyn@naver.com>
2025-06-30 22:57:35 +0900

    ddddd

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        
+      </head>
       <body className={inter.className}>
         {children}
         <BootstrapClient />
