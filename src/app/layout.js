51a8db02f772335dc791a1d5337adc53f7456350
hwang <anakyn@naver.com>
2025-06-30 22:41:03 +0900

    ㄹㄹㄹ

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import Script from 'next/script'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 
 export const metadata = {
@@ -11,9 +13,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
   <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"></link>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+            rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+            crossOrigin="anonymous" /> 
       <link href="/star/css/liderCustom.css" rel="stylesheet" type="text/css"></link>
       </head>
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossOrigin="anonymous" defer></script>
+    <Component {...pageProps} />
       <body>
         {children}
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
